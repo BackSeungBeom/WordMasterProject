02e942685254bee1fca2f8e104874a2222e66f4f
Back SeungBeom <seungbeom1234@gmail.com>
2023-09-02 19:15:38 +0900

    lecture2

--- src/main/java/com/mycom/word/WordManager.java
+++ src/main/java/com/mycom/word/WordManager.java
@@ -14,11 +14,31 @@ public class WordManager {
         while(true){
             int menu = selectMenu();
             if(menu==0) break;
+            if(menu==1){
+                wordCRUD.listAll();
+            }
+            if(menu==2){
+                //수준별 단어
+                //wordCRUD.
+            }
+            if(menu==3){
+                //검색
+                //wordCRUD.
+            }
             if(menu==4){
                 wordCRUD.addWord();
             }
-            if(menu==1){
-                wordCRUD.listAll();
+            if(menu==5){
+                //수정
+                wordCRUD.updateItem();
+            }
+            if(menu==6){
+                //삭제
+                //wordCRUD.
+            }
+            if(menu==7){
+                //파일 저장
+                //wordCRUD.
             }
         }
     }
@@ -28,8 +48,9 @@ public class WordManager {
                 +"********************\n"
                 +"1. 모든 단어 보기\n"
                 +"2. 수준별 단어 보기\n"
-                +"3. 단어 추가\n"
-                +"4. 단어 수정\n"
+                +"3. 단어 검색\n"
+                +"4. 단어 추가\n"
+                +"5. 단어 수정\n"
                 +"6. 단어 삭제\n"
                 +"7. 파일 저장\n"
                 +"0. 나가기\n"
